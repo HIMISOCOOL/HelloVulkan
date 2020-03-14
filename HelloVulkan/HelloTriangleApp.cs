07b48d6de6e667a775fdb8984cd8d9318bc3051a
Matthew Meehan <matusmarx@hotmail.com>
2020-03-14 18:25:19 +1300

    Hello Window

---
+++ HelloVulkan/HelloTriangleApp.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Drawing;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Common;
+
+namespace HelloVulkan 
+{
+    public class HelloTriangleApp
+    {
+        private IVulkanWindow _window;
+
+        public void Run()
+        {
+            initWindow();
+            InitVulkan();
+            MainLoop();
+            Cleanup();
+        }
+
+        private void initWindow()
+        {
+            var opts = WindowOptions.DefaultVulkan;
+            opts.Size = new Size(800, 600);
+            opts.WindowBorder = WindowBorder.Fixed;
+            opts.Title = "Vulkan";
+            _window = Window.Create(opts) as IVulkanWindow;
+            if (_window == null || !_window.IsVulkanSupported)
+            {
+                throw new NotSupportedException("Windowing does not Support Vulkan");
+            }
+        }
+
+        private void InitVulkan()
+        {
+
+        }
+
+        private void MainLoop()
+        {
+            _window.Run();
+        }
+
+        private void Cleanup()
+        {
+            
+        }
+    }
+}
---
+++ HelloVulkan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloVulkan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new HelloTriangleApp();
+            app.Run();
+        }
+    }
+}
