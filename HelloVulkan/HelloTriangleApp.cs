93c51edd2a5f61105fde9880c433537f8c6cd6ea
Matthew Meehan <matusmarx@hotmail.com>
2020-03-22 11:36:55 +1300

    Hello window vulkan complete

--- HelloVulkan/HelloTriangleApp.cs
+++ HelloVulkan/HelloTriangleApp.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
+using Silk.NET.Vulkan;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Common;
 
@@ -8,6 +10,8 @@ namespace HelloVulkan
     public class HelloTriangleApp
     {
         private IVulkanWindow _window;
+        private Vk _vk;
+        private Instance _instance;
 
         public void Run()
         {
@@ -33,7 +37,43 @@ namespace HelloVulkan
 
         private void InitVulkan()
         {
+            CreateInstance();
+        }
 
+        private unsafe void CreateInstance()
+        {
+            _vk = Vk.GetApi();
+
+            var appInfo = new ApplicationInfo
+            {
+                SType = StructureType.ApplicationInfo,
+                PApplicationName =  (byte*) Marshal.StringToHGlobalAnsi("Hello Triangle"),
+                ApplicationVersion = Vk.MakeVersion(1, 0),
+                PEngineName = (byte*) Marshal.StringToHGlobalAnsi("No Engine"),
+                EngineVersion = Vk.MakeVersion(1, 0),
+                ApiVersion = Vk.Version10
+            };
+
+            var createInfo = new InstanceCreateInfo
+            {
+                SType = StructureType.InstanceCreateInfo,
+                PApplicationInfo = &appInfo
+            };
+
+            char** extensions = _window.GetRequiredExtensions(out var extCount);
+            createInfo.EnabledExtensionCount = extCount;
+            createInfo.PpEnabledExtensionNames = (byte**) extensions;
+            createInfo.EnabledLayerCount = 0;
+
+            fixed(Instance* instance = &_instance)
+            {
+                var result = _vk.CreateInstance(&createInfo, null, instance);
+                if (result != Result.Success)
+                {
+                    throw new Exception("Failed to create instance!");
+                }
+            }
+            _vk.CurrentInstance = _instance;
         }
 
         private void MainLoop()
@@ -41,9 +81,9 @@ namespace HelloVulkan
             _window.Run();
         }
 
-        private void Cleanup()
+        private unsafe void Cleanup()
         {
-
+            _vk.DestroyInstance(_instance, (AllocationCallbacks*) null);
         }
     }
 }
